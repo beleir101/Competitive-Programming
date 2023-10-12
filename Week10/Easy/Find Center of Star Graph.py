ddb60a8ef88c4bbd6296c435feb82eab01d7c4ba
beleir101 <beleirmessay2436@gmail.com>
2023-10-12 15:44:50 +0300

    commit suicide

---
+++ Week10/Easy/Find Center of Star Graph.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        if edges[1].count(edges[0][0])>0:
+            return edges[0][0]
+        return edges[0][1]
--- Week10/Easy/Sources and sinks.py
+++ Week10/Easy/Sources and sinks.py
@@ -1,14 +1,25 @@
 import sys
-from collections import defaultdict
 c = int(sys.stdin.readline())
+
 games = []
 for i in range(c):
     d = list(map(int,sys.stdin.readline().split()))
     games.append(d)
-st = defaultdict(list)
+sources = list(range(1,c+1))
+sinks = []
 for j in range(c):
+    No = True
     for x in range(c):
         if games[j][x] == 1:
-            st[j+1].append(x+1)
-for v in st:
-    print(len(st[v]), *st[v])
+            No = False
+            sources.remove(x+1)
+    if No:
+        sinks.append(j+1)
+print(len(sources), end = " ")
+for i in sources:
+    print(i, end = " ")
+print()
+print(len(sinks), end = " ")
+for j in sinks:
+    print(j, end=" ")
+
