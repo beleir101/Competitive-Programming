951f6ee9014ad43d328cc4514ab9c73282d5b4ef
beleir101 <beleirmessay2436@gmail.com>
2023-12-03 07:43:05 +0300

    Commit Suicide
    Signed-off-by: beleir101 <beleirmessay2436@gmail.com>

--- Week 12/Easy/Longest Common Prefix.py
+++ Week 12/Easy/Longest Common Prefix.py
@@ -4,7 +4,7 @@ class Solution:
         strs.sort(key = lambda x:len(x))
         for i in range(len(strs[0])):
             for x in strs:
-                if x[i] != strs[0][i]
+                if x[i] != strs[0][i]:
                     return lon
             lon += strs[0][i]
         return lon
